f98838003970551aca6ce07dc31e5daa0239c74a
barkacode <unlimitediq@proton.me>
2025-06-27 16:38:47 +0200

    feat: refactor product listing with DataTable and dropdown menu integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import CreateProductForm from "@/components/forms/createProductForm";
 import AddProduct from "@/components/Product/addProduct";
-import ProductCreatedList from "@/components/Product/ProductCreatedList";
+import { DataTable } from "@/components/ui/data-table";
 import { mockProducts } from "@/lib/data";
+import { columns } from "./products/columns";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:px-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col w-full gap-[32px]">
-        <ProductCreatedList products={mockProducts} />
+        <h2 className="text-xl font-bold">Mes fiches produits</h2>
+        <DataTable columns={columns} data={mockProducts} />
         <div className="w-full flex justify-end ">
           <AddProduct />
         </div>
