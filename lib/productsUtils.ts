dc140ff70802d785693987a2df998b1936e022dc
ilès <iles@mentorgoal.com>
2025-06-27 10:44:50 +0200

    format description

---
+++ lib/productsUtils.ts
@@ -0,0 +1,22 @@
+import { Product, products } from "./products";
+
+export function getPoidsByProductName(productName: string) {
+	const product: Product = products[productName];
+	if (product) {
+		return product.poids;
+	} else {
+		throw new Error("Product not found");
+	}
+}
+
+export function getDescriptionByProductName(productName: string) {
+	const product = products[productName];
+	if (product) {
+		return product.description;
+	} else {
+		throw new Error("Product not found");
+	}
+}
+
+console.log(getPoidsByProductName("tshirt_adjusted")); // Outputs: 180
+console.log(getDescriptionByProductName("tshirt_adjusted")); // Outputs: T-shirt col rond manches courtes.\nCoupe : Ajusté\nModèle
