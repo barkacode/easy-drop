b36f8890a9adbc7ec225474fd4c4b8b5b61add03
barkacode <unlimitediq@proton.me>
2025-07-15 12:09:20 +0200

    refactor: simplifier la logique d'authentification dans le middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
-export async function middleware(req: NextRequest) {
+export function middleware(req: NextRequest) {
   const { nextUrl } = req;
   const currentPath = nextUrl.pathname;
 
   const sessionCookie = getSessionCookie(req);
   const isAuthenticated = !!sessionCookie;
-
-  if (!isAuthenticated && currentPath !== "/auth/signin") {
-    return NextResponse.redirect(new URL("/auth/signin", req.url));
-  }
 
   if (currentPath === "/auth/signin" && isAuthenticated) {
     return NextResponse.redirect(new URL("/", req.url));
